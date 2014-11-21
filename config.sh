dc8cd0bcd041b0780429b9911a4858194a64498c
nyaff <nyaff@users.noreply.github.com>
2014-11-21 16:28:11 +0000

    Use example.com instead of an actual domain name
    Use 'example.com' (a domain name reserved for documentation and other
    purposes, see RFC 2606 for details) in the default email configuration
    section instead of an actual domain name in case this setting is not
    altered by the user.

--- config.sh
+++ config.sh
@@ -16,8 +16,8 @@
 # Mail configuration
 ################################################
 
-CFG_MAIL_FROM="nas@isp.com"	# Enter the mail address of the NAS here (should be the same address than in NAS GUI: System|Advanced|Email)
-CFG_MAIL_TO="admin@isp.com"	# Enter the email of the NAS administrator here
+CFG_MAIL_FROM="nas@example.com"	# Enter the mail address of the NAS here (should be the same address than in NAS GUI: System|Advanced|Email)
+CFG_MAIL_TO="admin@example.com"	# Enter the email of the NAS administrator here
 
 # Paths to log folder, temp folder...
 #
